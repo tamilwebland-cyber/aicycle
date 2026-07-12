187f7f2f92fc32f090cc975c9caf3045c45e95c9
tamilwebland-cyber <tamilwebland@gmail.com>
2026-07-12 14:44:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+// Aicycle Live Search
 
+const search = document.getElementById("search");
+
+if (search) {
+  search.addEventListener("keyup", function () {
+
+    const value = this.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+      const text = card.innerText.toLowerCase();
+
+      if (text.includes(value)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+
+    });
+
+  });
+}

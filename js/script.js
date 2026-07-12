332de1b29e522b9ba88c76dedaf101836bf04111
tamilwebland-cyber <tamilwebland@gmail.com>
2026-07-12 20:49:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+const search = document.getElementById("search");
 
-  const search = document.getElementById("search");
-
-  if (!search) {
-    alert("Search input NOT found");
+if (!search) {
     return;
-  }
+}
 
-  alert("Search input found");
+const cards = document.querySelectorAll(".card");
 
-  const cards = document.querySelectorAll(".card");
-
-  alert("Cards: " + cards.length);
-
-  search.addEventListener("input", function () {
-
+search.addEventListener("input", function () {
     const value = this.value.toLowerCase();
 
     cards.forEach(function(card) {
-
-      const text = card.textContent.toLowerCase();
-
-      card.style.display = text.includes(value) ? "" : "none";
-
+        const text = card.textContent.toLowerCase();
+        card.style.display = text.includes(value) ? "" : "none";
     });
-
-  });
+});
 
 });
